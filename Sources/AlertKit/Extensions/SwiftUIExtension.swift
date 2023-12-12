f55b733a1f28c8d84daeaec72efac5e79f927c6b
tkgstrator <nasawake.am@gmail.com>
2023-12-13 02:42:17 +0900

    fix content

--- Sources/AlertKit/Extensions/SwiftUIExtension.swift
+++ Sources/AlertKit/Extensions/SwiftUIExtension.swift
@@ -3,14 +3,14 @@ import SwiftUI
 @available(iOS 13.0, *)
 extension View {
     
-    public func alert(isPresented: Binding<Bool>, content: AlertViewProtocol, completion: (()->Void)? = nil) -> some View {
+    public func alert(isPresented: Binding<Bool>, @ViewBuilder content: @escaping () -> AlertViewProtocol, completion: (()->Void)? = nil) -> some View {
         if isPresented.wrappedValue {
             let wrapperCompletion = {
                 isPresented.wrappedValue = false
                 completion?()
             }
             if let window = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first {
-                content.present(on: window, completion: wrapperCompletion)
+                content().present(on: window, completion: wrapperCompletion)
             }
         }
         return self
