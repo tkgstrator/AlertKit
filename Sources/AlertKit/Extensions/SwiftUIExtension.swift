ebab1123a538c78deb00149b8b6f3767b745ecf9
tkgstrator <nasawake.am@gmail.com>
2023-12-12 23:18:02 +0900

    rename view to content

--- Sources/AlertKit/Extensions/SwiftUIExtension.swift
+++ Sources/AlertKit/Extensions/SwiftUIExtension.swift
@@ -3,14 +3,14 @@ import SwiftUI
 @available(iOS 13.0, *)
 extension View {
     
-    public func alert(isPresented: Binding<Bool>, view: AlertViewProtocol, completion: (()->Void)? = nil) -> some View {
+    public func alert(isPresented: Binding<Bool>, content: AlertViewProtocol, completion: (()->Void)? = nil) -> some View {
         if isPresented.wrappedValue {
             let wrapperCompletion = {
                 isPresented.wrappedValue = false
                 completion?()
             }
             if let window = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first {
-                view.present(on: window, completion: wrapperCompletion)
+                content.present(on: window, completion: wrapperCompletion)
             }
         }
         return self
